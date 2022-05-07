741be8e54d2f6ce86b86bb74c81ec17b0e5b4588
adictonator <adityabhaskarsharma@gmail.com>
2022-05-08 02:21:27 +0530

    initial commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export var speed: int = 400
+export var gravity: int = 30
+var velocity: Vector2 = Vector2.ZERO
+var jumpForce: int = 850
+
+func _physics_process(delta: float) -> void:
+	#velocity = Vector2.ZERO
+	if Input.is_action_pressed('move_left'):
+		velocity.x = -speed
+		$AnimatedSprite.flip_h = true
+	elif Input.is_action_pressed('move_right'):
+		velocity.x = speed
+		$AnimatedSprite.flip_h = false
+	else:
+		pass
+		#$AnimatedSprite.play('idle')
+
+	velocity.y += gravity
+
+	#if !is_on_floor():
+	#	$AnimatedSprite.play('jump')
+
+	if is_on_floor() && Input.is_action_just_pressed('jump'):
+		velocity.y = -jumpForce
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(velocity.x, 0, 0.3)
